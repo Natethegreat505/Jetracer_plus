c40e395a61290e8db033096903b3867f1df3c0d9
Natethegreat505 <125489092+Natethegreat505@users.noreply.github.com>
2023-04-27 08:13:31 -0600

    Update steering_command_argument.py

--- code/steering_command_argument.py
+++ code/steering_command_argument.py
@@ -1,8 +1,10 @@
-#4/21/23
+#4/27/23
 from jetracer.nvidia_racecar import nvidiaracecar
 import argparse
 car = nvidia_racecar()
 parser = argparse.ArgumentParser()
-parser.add_argument('--value', type+float, required=True)
+parser.add_argument('--value', type=float, required=True)
 args = parser.parse_args()
 car.steering = args.value
+
+print = ("the command sever value is: ", args.value)
